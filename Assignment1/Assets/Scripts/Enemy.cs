40290f48edf1e9aa6ed93d848cc5ecd044097b5b
Isaac <ikingst7@gmail.com>
2017-01-22 11:30:33 -0700

    Enemy/Player movement
    Used NavMeshAgents to let the enemy and player move.

--- Assignment1/Assets/Scripts/Enemy.cs
+++ Assignment1/Assets/Scripts/Enemy.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : Entity {
 
-	// call constructor
-	// change methods on how it attacks and moves
+	// Let the enemy attack when within a range
+	// The enemy will not follow you until within a range.
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	void Start() {
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update() {
+		agent.SetDestination(target.position);
+	}
 }
--- Assignment1/Assets/Scripts/Player.cs
+++ Assignment1/Assets/Scripts/Player.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Player : Entity {
 
-	// call constructor
-	// change methods on how it attacks and moves
+	// let the player attack
+	// I want to eventually move with wasd, but this works for now.
+	UnityEngine.AI.NavMeshAgent agent;
+        
+        void Start() {
+            agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        }
+        
+        void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                RaycastHit hit;
+                
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                    agent.destination = hit.point;
+                }
+            }
+        }
 }
